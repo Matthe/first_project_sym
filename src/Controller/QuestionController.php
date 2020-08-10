2092aabf3ad1281c256f4ec82bb824900aa19bb0
Matthé Jacobs <m_jacobs@hotmail.nl>
2020-08-10 20:00:47 +0200

    Used Twig and made a question test page

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\AbstractServiceConfigurator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,8 +22,15 @@ class QuestionController
      */
 	public function show($slug)
     {
-        return new Response(sprintf(
-            'If you can jump very high maybe you can fly "%s"',  ucwords(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Dit is antwoord op vraag 1',
+            'Hier nog wat tekst om het te testen',
+            'Bananen zijn krom want ze groeien naar het lichts'
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
