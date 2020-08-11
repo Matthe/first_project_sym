cab7ff5b87762b2ede92619aea8f3a76e3a7d516
Matthé Jacobs <m_jacobs@hotmail.nl>
2020-08-11 11:06:45 +0200

    small commit before install

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,14 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Loader\Configurator\AbstractServiceConfigurator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-	public function homepage()
+	public function homepage(Environment $twigEnvironment)
 	{
+	    /*
+	    //Example of using the Twig service directly
+	    $html = $twigEnvironment->render('question/homepage.html.twig');
+
+	    return new Response($html);
+	    */
 		return $this->render('question/homepage.html.twig');
 	}
 
